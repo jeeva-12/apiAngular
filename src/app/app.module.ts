c5b5b4df3a90a2ff2def47e0870983f7beddebd6
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-29 16:41:52 +0530

    htf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,34 @@ import { PassengerComponent } from './passenger/passenger.component';
 import { UsDataComponent } from './us-data/us-data.component';
 import { PublicComponent } from './public/public.component';
 import { InfoComponent } from './info/info.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+import { QuoteComponent } from './quote/quote.component';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:PassengerComponent
+  },
+  {
+    path:"us",
+    component:UsDataComponent
+  },
+  {
+    path:"public",
+    component:PublicComponent
+  },
+  {
+    path:"user",
+    component:UserDataComponent
+  },
+  {
+    path:"todo"
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,11 +42,17 @@ import { InfoComponent } from './info/info.component';
     PassengerComponent,
     UsDataComponent,
     PublicComponent,
-    InfoComponent
+    InfoComponent,
+    ShoppingComponent,
+    UserDataComponent,
+    TodoComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
