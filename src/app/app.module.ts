8285f80abce4aaedad6a6713b18c9e19f921a920
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-29 09:53:30 +0530

    hdhg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerComponent } from './passenger/passenger.component';
+import { UsDataComponent } from './us-data/us-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerComponent,
+    UsDataComponent
   ],
   imports: [
     BrowserModule,
